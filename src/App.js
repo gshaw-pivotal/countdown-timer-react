7271c98daa00338a71429d355a7cb2c40113ba17
Gavin Shaw <gavshaw+github@gmail.com>
2024-08-17 00:09:40 -0400

    Cetup countdown timer with auto start of 1 hour
    - Populating the hour, minute and second inputs and clicking the button
      causes the timer to be reset to the entered time and start counting
      down from the provided value instead of the default.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
+import React, { useState, useEffect } from 'react';
+
+const CountdownTimer = () => {
+    const [timeRemaining, setTimeRemaining] = useState(60 * 60);
+
+    const [totalHours, setTotalHours] = useState(0);
+    const [totalMinutes, setTotalMinutes] = useState(0);
+    const [totalSeconds, setTotalSeconds] = useState(0);
+
+    const updateCountdown = event => {
+        setTimeRemaining((totalHours * 60 * 60) + (totalMinutes * 60) + totalSeconds)
+    }
+
+    useEffect(() => {
+        const timerInterval = setInterval(() => {
+            setTimeRemaining((prevTime) => {
+                if (prevTime === 0) {
+                    clearInterval(timerInterval);
+                    return 0;
+                } else {
+                    return prevTime - 1;
+                }
+            });
+        }, 1000);
+
+        return () => clearInterval(timerInterval);
+    }, []);
+
+    // Convert seconds to hours, minutes, and seconds
+    const hours = Math.floor(timeRemaining / 3600);
+    const minutes = Math.floor((timeRemaining % 3600) / 60);
+    const seconds = timeRemaining % 60;
+
+    return (
+        <div align="center">
+            <div>
+            <input
+                type="number"
+                onChange={e => setTotalHours(e.target.value)}
+                value={totalHours}
+            />
+            <span>hours</span>
+            <input
+                type="number"
+                onChange={e => setTotalMinutes(e.target.value)}
+                value={totalMinutes}
+            />
+            <span>minutes</span>
+            <input
+                type="number"
+                onChange={e => setTotalSeconds(e.target.value)}
+                value={totalSeconds}
+            />
+            <span>seconds</span>
+            </div>
+            <div>
+                <button onClick={updateCountdown}>Set & Restart Timer</button>
+            </div>
+            <hr/>
+            <h2>Time Remaining:</h2>
+            <h1>{`${hours}h ${minutes}m ${seconds}s`}</h1>
+        </div>
+    );
+};
+
 function App() {
     return (
         <div>
-
+            <CountdownTimer/>
         </div>
     );
 }
